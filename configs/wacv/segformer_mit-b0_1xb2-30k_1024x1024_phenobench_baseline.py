6af81ed253cfe524eaf610d98878e372737a7473
Naeem Iqbal <naeemiqbal996@gmail.com>
2026-06-04 00:41:30 +0200

    Put all experiments into one folder

---
+++ configs/wacv/segformer_mit-b0_1xb2-30k_1024x1024_phenobench_baseline.py
@@ -0,0 +1,73 @@
+_base_ = [
+    '../_base_/models/segformer_mit-b0.py',
+    '../_base_/datasets/phenobench3.py',
+    '../_base_/default_runtime.py',
+]
+crop_size = (1024, 1024)
+num_iters = 30000
+interval = 1000
+num_classes = 3
+data_preprocessor = dict(size=crop_size)
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b0_20220624-7e0fe6dd.pth'  # noqa
+
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        init_cfg=dict(
+            type='Pretrained', 
+            checkpoint=checkpoint)
+    ),
+    decode_head=dict(
+        num_classes=num_classes)
+)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=500,
+        end=num_iters,
+        by_epoch=False,
+    )
+]
+
+train_cfg = dict(
+    type='IterBasedTrainLoop', 
+    max_iters=num_iters, 
+    val_interval=interval)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(
+        type='CheckpointHook', 
+        by_epoch=False, 
+        interval=interval,
+        max_keep_ckpts=1,
+        save_best='IoU_weed',
+        rule='greater'
+    ),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook')
+)
+
+# auto_scale_lr = dict(enable=False, base_batch_size=8)
+# train_dataloader = dict(batch_size=1, num_workers=4)
+# val_dataloader = dict(batch_size=1, num_workers=4)
+# test_dataloader = val_dataloader
